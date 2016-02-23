5df5c3b18cd037eac8c87b7a002f9b5594db8d39
Lawrence Buss <LawSBuss@gmail.com>
2016-02-23 12:30:34 -0800

    rabbitmq/celery task queue running

--- runserver.py
+++ runserver.py
@@ -1,14 +1,34 @@
-from flask import Flask, request, redirect, jsonify, url_for
+from flask import Flask, request, redirect, jsonify, url_for, g
 from utils import *
 import copy
+from random import randint
 from threading import Thread
 from werkzeug import secure_filename
 from modules.exposure.exposure_mod import ExposureMod as Exposure
 from modules.qsar.qsar_mod import QSARmod as QSAR
 from modules.lcia.net_prediction import NetPrediction as LCIA
 from modules.ft.fate_and_transport_lvl4 import FateAndTransport as FAT
+from celery import Celery
+
 
 app = Flask(__name__)
+app.config.update(
+    CELERY_BROKER_URL='amqp://guest:guest@localhost:5672//'
+)
+
+def make_celery(app):
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+celery = make_celery(app)
 
 ALLOWED_EXTENSIONS = set(['txt'])
 exp = Exposure()
@@ -16,9 +36,23 @@ qsar = QSAR()
 lcia = LCIA()
 fat = FAT()
 
+app.tickets = {}
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def after_this_request(func):
+    if not hasattr(g, 'call_after_request'):
+        g.call_after_request = []
+    g.call_after_request.append(func)
+    return func
+
+@app.after_request
+def per_request_callbacks(response):
+    for func in getattr(g, 'call_after_request', ()):
+        response = func(response)
+    return response
 
 @app.route('/run_job', methods=['POST','GET'])
 def run_job():
@@ -64,9 +98,6 @@ def run_job():
             'qsar': qsar_results
             }})
 
-@app.route('/qsar_batch', methods=['POST'])
-def qsar_batch():
-    # import pdb; pdb.set_trace()
 # deprecated until basic functionality is more polished
 # @app.route('/upload_epi_result', methods=['POST'])
 # def upload_file():
@@ -86,9 +117,6 @@ def qsar_batch():
 #     query = request.form['query']
 #     chem = chem_spider.get_chem(query)
 #     return jsonify(chem)
-
-
-
 
 
 # Test routes using seed data
@@ -118,9 +146,60 @@ def run_lcia_default_values():
 
 @app.route('/ft_exposure_test', methods=['GET'])
 def test_ft_exposure():
-        fat_outs = fat.run({})
-        exposure_results = exp.run(fat_outs['exposure_inputs'])
-        return jsonify({'results': exposure_results})
+    fat_outs = fat.run({})
+    exposure_results = exp.run(fat_outs['exposure_inputs'])
+    return jsonify({'results': exposure_results})
+
+def test_run():
+    qsar_results = qsar.run()[0]
+    chem = copy.copy(qsar_results)
+    print(chem)
+    fat_results = fat.run()
+    chem.update(fat_results['exposure_inputs'])
+    exposure_results = exp.run(chem)
+    return {'results':  {
+        'exposure': exposure_results,
+        'fat': fat_results['fat_outputs'],
+        'qsar': qsar_results
+        }}
+
+
+@celery.task(name='__main__.run_async_job')
+def run_async_job(smiles, ticket):
+    print "running celery task."
+    chemicals = test_run()
+    if chemicals:
+        app.tickets[ticket] = chemicals
+    else:
+        app.tickets[ticket] = 'Run Failed'
+
+@app.route('/get_ticket', methods=['POST'])
+def give_ticket():
+    smiles = {'smiles_in': request.form['smiles']}
+    ticket = None
+    while not ticket or ticket in app.tickets:
+        ticket = str(randint(1, 9999))
+    app.tickets[ticket] = None
+    run_async_job.delay(smiles, ticket)
+    return ticket
+
+
+@app.route('/check_ticket', methods=['POST'])
+def check_ticket():
+    ticket = str(request.form['ticket'])
+    print ticket
+    print app.tickets
+    try:
+        if app.tickets[ticket]:
+            result = app.tickets.pop(ticket, None)
+            return result
+        elif ticket in app.tickets:
+            return "Not finished."
+    except:
+        return "Ticket not found."
+
+
 
 if __name__ == '__main__':
     app.run(debug = True)
+    # celery -A your_application worker
